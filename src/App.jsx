f3951d3efaf26ecf0e1e6e1ee158c5f16b57bd18
Pranav Srivastva <pranav.srivastava.1291@gmail.com>
2024-05-28 11:26:26 +0530

    swiperjs installed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../src/components/Navbar.jsx';
 import Hero from '../src/components/Hero.jsx'
 import './index.css'
-import AnimatedElement from './components/animate.jsx';
+
 
 const App = () => {
   return (
